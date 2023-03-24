8d1c5be310b4ad53a4c7a0b2a1dac24423a2e72f
majewskibartosz <majewskibartosz@pm.me>
2023-03-24 09:31:52 +0100

    fix[part1] - change functions and sequencer code to new js

--- scripts/functions.js
+++ scripts/functions.js
@@ -2,70 +2,78 @@
 // ==============================================================
 // METHODS
 // ==============================================================
-let hasSwing = false
-function toggleButton(button) {
+let hasSwing = false;
+
+const toggleButton = (button) => {
   // setup swing button behaviour
   if (button.classList.contains('swing')) {
     if (button.value === 'OFF') {
-      Tone.Transport.swing = 0.25
-      button.value = 'ON'
-      button.textContent = 'SWING ON\xa0'
-      hasSwing = true
+      Tone.Transport.swing = 0.25;
+      button.value = 'ON';
+      button.textContent = 'SWING ON\xa0';
+      hasSwing = true;
     } else {
-      Tone.Transport.swing = 0
-      button.value = 'OFF'
-      button.textContent = 'SWING OFF'
-      hasSwing = false
+      Tone.Transport.swing = 0;
+      button.value = 'OFF';
+      button.textContent = 'SWING OFF';
+      hasSwing = false;
     }
-    return hasSwing
-  } else { // setup play/pause button behaviour
-    const playStopBtn = document.querySelector('button > i')
+    return hasSwing;
+  } else {
+    // setup play/pause button behaviour
+    const playStopBtn = document.querySelector('button > i');
     if (button.value === 'ON') {
-      Tone.Transport.stop()
-      button.value = 'OFF'
-      playStopBtn.textContent = 'pause'
+      Tone.Transport.stop();
+      button.value = 'OFF';
+      playStopBtn.textContent = 'pause';
     } else {
-      Tone.Transport.start()
-      button.value = 'ON'
-      playStopBtn.textContent = 'play_arrow'
+      Tone.Transport.start();
+      button.value = 'ON';
+      playStopBtn.textContent = 'play_arrow';
     }
   }
-}
+};
 
 // Add or remove class - depends on swing status
-function modifyHighlightClass(el) {
+const modifyHighlightClass = (el) => {
   if (!hasSwing) {
-    modifyClass(el, 'add', 'highlight')
-    setTimeout(() => { // Delete highlight class after specified time
-      modifyClass(el, 'remove', 'highlight')
-    }, 120)
+    modifyClass(el, 'add', 'highlight');
+    setTimeout(() => {
+      modifyClass(el, 'remove', 'highlight');
+    }, 120);
   } else {
-    modifyClass(el, 'add', 'highlight-swing')
-    setTimeout(() => { // Delete highlight-swing class after specified time
-      modifyClass(el, 'remove', 'highlight-swing')
-    }, 120)
+    modifyClass(el, 'add', 'highlight-swing');
+    setTimeout(() => {
+      modifyClass(el, 'remove', 'highlight-swing');
+    }, 120);
   }
-}
+};
 
-function modifyClass(el, action, elClass) {
-  action === 'add' ? el.classList.add(elClass)
-    : action === 'remove' ? el.classList.remove(elClass)
-      : el.classList.toggle(elClass)
-}
+const modifyClass = (el, action, elClass) => {
+  if (action === 'add') {
+    el.classList.add(elClass);
+  } else if (action === 'remove') {
+    el.classList.remove(elClass);
+  } else {
+    el.classList.toggle(elClass);
+  }
+};
 
-function singleClick(el) {
-  el = el.target
-  modifyClass(el, 'toggle', 'clicked')
-  modifyClass(el, 'remove', 'dbl-clicked')
-}
+const singleClick = (el) => {
+  el = el.target;
+  modifyClass(el, 'toggle', 'clicked');
+  modifyClass(el, 'remove', 'dbl-clicked');
+};
 
-function doubleClick(el) {
-  el = el.target
-  modifyClass(el, 'toggle', 'dbl-clicked')
-  modifyClass(el, 'remove', 'clicked')
-}
+const doubleClick = (el) => {
+  el = el.target;
+  modifyClass(el, 'toggle', 'dbl-clicked');
+  modifyClass(el, 'remove', 'clicked');
+};
 
 export {
-  toggleButton, modifyHighlightClass,
-  singleClick, doubleClick
-}
+  toggleButton,
+  modifyHighlightClass,
+  singleClick,
+  doubleClick,
+};
--- scripts/sequencer.js
+++ scripts/sequencer.js
@@ -26,30 +26,31 @@ kit.connect(gain)
 // ==============================================================
 // SEQUENCER SETUP
 // ==============================================================
-const rows = document.body.querySelectorAll('div > div')
+const rows = [...document.body.querySelectorAll('div > div')];
 const sounds = ['C2', 'D2', 'E2', 'F2', 'G2', 'C3', 'D3', 'G3']
 let index = 0
 
 const velocity = (Math.random() * 0.5 + 0.5) // randomize velocity of each step
 //  MAIN SEQUENCER LOOP
-function repeat(time) {
-  const step = index % 16
-  for (let i = 0; i < rows.length; i++) {
-    const sound = sounds[i]
-    const row = rows[i]
-    const input = row.querySelector(`span:nth-child(${step + 1})`)
-    const hasClicked = input.classList.contains('clicked')
-    const hasDoubleClicked = input.classList.contains('dbl-clicked')
+const repeat = (time) => {
+  const step = index % 16;
+  rows.forEach((row, i) => {
+    const sound = sounds[i];
+    const input = [...row.children][step];
+    const hasClicked = input.classList.contains('clicked');
+    const hasDoubleClicked = input.classList.contains('dbl-clicked');
+    
     if (hasClicked) {
-      kit.triggerAttack(sound, time, velocity)
+      kit.triggerAttack(sound, time, velocity);
     } else if (hasDoubleClicked) {
-      kit.triggerAttack(sound, time, velocity / 2)
+      kit.triggerAttack(sound, time, velocity / 2);
     }
-    Tone.Draw.schedule(() => { // Draw sequencer movement
-      modifyHighlightClass(input)
-    }, time)
-  }
-  index++
-}
+
+    Tone.Draw.schedule(() => {
+      modifyHighlightClass(input);
+    }, time);
+  });
+  index++;
+};
 
 export { repeat }
